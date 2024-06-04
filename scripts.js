774b63aeb1b226c290c67fda8121175e86789e42
MustuBharmal <mustafabharmal10@gmail.com>
2024-06-04 14:13:02 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Basic script to handle smooth scrolling for the navbar links
+document.querySelectorAll('.sticky-navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
