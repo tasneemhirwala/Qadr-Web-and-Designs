2210550ae633dc4742e150906b020e245950ca88
MustuBharmal <mustafabharmal10@gmail.com>
2024-06-04 19:54:32 +0530

    second commit

--- scripts.js
+++ scripts.js
@@ -7,4 +7,12 @@ document.querySelectorAll('.sticky-navbar a').forEach(anchor => {
       });
     });
   });
+  document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
   
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('active');
+      burger.classList.toggle('toggle');
+    });
+  });
